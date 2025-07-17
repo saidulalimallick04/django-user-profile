975f1e064afc07be5eaaac6e648556caa901e6f6
saidulalimallick04 <saidulalimallick1@gmail.com>
2025-07-17 14:37:51 +0530

    fix static files

--- projectroot/settings.py
+++ projectroot/settings.py
@@ -156,8 +156,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = (BASE_DIR / 'staticfiles')
-
+STATIC_ROOTS = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
